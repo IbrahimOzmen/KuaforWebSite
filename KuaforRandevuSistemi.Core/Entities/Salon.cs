ae7c90417247d98f8df56216281fae2b39820c59
Ibrahim Ozmen <ibrahm.ozmen2@gmail.com>
2024-12-13 19:09:54 +0300

    İşletme ve hizmet modelleri eklendi: Salon ve Servis entity'leri oluşturuldu

---
+++ KuaforRandevuSistemi.Core/Entities/Salon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using KuaforRandevuSistemi.Core.Entities.Common;
+
+namespace KuaforRandevuSistemi.Core.Entities
+{
+    public class Salon : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public TimeSpan OpeningTime { get; set; }
+        public TimeSpan ClosingTime { get; set; }
+        public bool IsActive { get; set; }
+
+        public virtual ICollection<Service> Services { get; set; }
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+}
---
+++ KuaforRandevuSistemi.Core/Entities/Service.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using KuaforRandevuSistemi.Core.Entities.Common;
+
+namespace KuaforRandevuSistemi.Core.Entities
+{
+    public class Service : BaseEntity
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Duration { get; set; }
+        public int SalonId { get; set; }
+
+        public virtual Salon? Salon { get; set; }
+        public virtual ICollection<EmployeeService> EmployeeServices { get; set; } = new List<EmployeeService>();
+        public virtual ICollection<Appointment> Appointments { get; set; } = new List<Appointment>();
+    }
+}
