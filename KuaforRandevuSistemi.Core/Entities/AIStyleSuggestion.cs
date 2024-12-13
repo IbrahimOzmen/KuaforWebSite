e1a7a305c166a4d47a18896933e68a620e315457
Ibrahim Ozmen <ibrahm.ozmen2@gmail.com>
2024-12-13 19:11:04 +0300

    AI özelliği eklendi: Saç modeli önerisi entitysi oluşturuldu

---
+++ KuaforRandevuSistemi.Core/Entities/AIStyleSuggestion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using KuaforRandevuSistemi.Core.Entities.Common;
+
+namespace KuaforRandevuSistemi.Core.Entities
+{
+    public class AIStyleSuggestion : BaseEntity
+    {
+        public int UserId { get; set; }
+        public string ImagePath { get; set; }
+        public string Suggestions { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public virtual AppUser User { get; set; }
+    }
+}
